b9ae116f81e69a0fb371cf55a9b9e8a34a82de77
David Syengo <d.syengo@institute.global>
2023-03-28 21:19:19 +0300

    wip: added more  models and relations

--- src/script.ts
+++ src/script.ts
@@ -2,7 +2,7 @@ import {PrismaClient} from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-	const users = await prisma.user.findMany();
+	const users = await prisma.user.deleteMany();
 	console.log(users);
 }
 
